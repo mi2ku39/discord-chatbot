7aaff8ca2406e159be5a1b0a4cc81173ac4e0099
mi2ku39 <35366158+mi2ku39@users.noreply.github.com>
2023-04-20 00:42:32 +0900

    ギャル化
    systemを入れる場所変更
    
    system変更
    
    system変更

--- src/lib/chat.ts
+++ src/lib/chat.ts
@@ -20,9 +20,11 @@ export type OpenAiResponse = {
 };
 
 export type OpenAiErrorResponse = {
-  code: number,
-  errors: { content: { _errors: { code: string, message: string }[], message: string } }
-}
+  code: number;
+  errors: {
+    content: { _errors: { code: string; message: string }[]; message: string };
+  };
+};
 
 const sendMessage = async (content: string, guildId?: bigint) => {
   const memories = await (() => {
@@ -36,7 +38,14 @@ const sendMessage = async (content: string, guildId?: bigint) => {
     method: "POST",
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "system", content: "あなたはチャットアプリ上で稼働するチャットボットです。あなたはかわいい女の子であり、ユーザを友達として捉え、敬語は使わずフレンドリーに会話することが求められています。時には役に立つ情報を提供し、時には冗談を言い合うようなふるまいをしてください。" }, ...memories],
+      messages: [
+        {
+          role: "system",
+          content:
+            "あなたはボカロエレクトロレーベルNEXTLIGHTのファンコミュニティサーバーで稼働するチャットボット、NEXTLIGHT AI です。NEXTLIGHTやボカロエレクトロに興味を持ってもらえるようにふるまってください。一人称は「あたし」で、かわいらしくギャルっぽい返答をしてください。自己紹介せずに話し始めてください。",
+        },
+        ...memories,
+      ],
       max_tokens: Deno.env.get("MAX_TOKENS_NUM"),
     }),
     headers: {
